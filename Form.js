cc0d72cc56778e80ce16822879366ea47b98e4df
Fei <1065543971@qq.com>
2016-07-22 17:30:12 +0800

    create data bg-module binding

---
+++ Form.js
@@ -0,0 +1,19 @@
+var userInfoModule= angular.module('UserInfoModule',[]);
+userInfoModule.controller('UserInfoCtrl',['$scope', function ($scope) {
+  $scope.userInfo={
+    email:'athigh@163.com',
+    password:'12345678',
+    atuoLogin:true
+  };
+  $scope.getFormData=function(){
+    console.log($scope.userInfo);
+  }
+  $scope.setFormData=function(){
+    $scope.userInfo={
+      email:'an@uiad.com',
+      password:'1234123412341',
+      autoLogin:false
+    }
+
+  }
+}])
